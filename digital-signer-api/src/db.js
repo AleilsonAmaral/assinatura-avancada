265f3dd5685c7916a202129bb1dc0623056bcfa3
Aleilson Amaral <aleilsonamaral470@gmail.com>
2025-10-29 11:54:41 -0300

    Força o cliente PG a usar IPv4 para resolver ENETUNREACH no Heroku.

--- digital-signer-api/src/db.js
+++ digital-signer-api/src/db.js
@@ -5,7 +5,8 @@ const { Pool } = require('pg');
 // 1. Definição do Pool de Conexão
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
-    ssl: { rejectUnauthorized: false } 
+    ssl: { rejectUnauthorized: false },
+    family: 4, // Força o uso de IPv4
 });
 
 // 2. Exportação PURA e imediata
